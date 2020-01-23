33743dbf2ea500ece05b80f8c0e22d7b8680528d
Kiran Bisht <55168312+8kiran8bisht@users.noreply.github.com>
2020-01-23 18:26:03 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,411 @@
-/** Add any JavaScript you need to this file. */
+/*All categories and data*/
+var tree = [
+    {
+        category: "tree1",
+        alt: "Christmas Tree",
+        url: "images/t6.jpg",
+        price: "CAD 99.5",
+        description: "7feet Green Tree: Medium Decoration"
+    },
+    {
+        category: "tree2",
+        alt: " Christmas Tree",
+        url: "images/t5.jpg",
+        price: "CAD 89",
+        description: "7feet White Tree: Medium Decoration"
+    },
+    {
+        category: "tree3",
+        alt: " Christmas Tree",
+        url: "images/t3.jpg",
+        price: "CAD 125",
+        description: "10feet Green Tree: Medium Decoration"
+    },
+    {
+        category: "tree1",
+        alt: "Christmas Tree",
+        url: "images/t2.jpg",
+        price: "CAD 110",
+        description: "10feet White Tree: Rich Decoration"
+    },
+    {
+        category: "tree2",
+        alt: " Christmas Tree",
+        url: "images/t4.jpg",
+        price: "CAD 60",
+        description: "5feet Green Tree: No Decoration"
+    },
+    {
+        category: "tree3",
+        alt: " Christmas Tree",
+        url: "images/t1.jpg",
+        price: "CAD 75",
+        description: "5feet Green Tree: Medium Decoration"
+    }
+];
+var lights = [
+    {
+        category: "l1",
+        alt: "Christmas Light",
+        url: "images/l1.jpg",
+        price: "CAD 15",
+        description: "20 feet Colorful Pointed LED lights"
+    },
+    {
+        category: "l2",
+        alt: " Christmas Light",
+        url: "images/l2.jpg",
+        price: "CAD 20",
+        description: "20 feet Colorful LED lights: Round "
+    },
+    {
+        category: "l4",
+        alt: "Christmas Light",
+        url: "images/l4.jpg",
+        price: "CAD 15",
+        description: "Pack of 5 Star shapes Decoration Light"
+    },
+    {
+        category: "l3",
+        alt: " Christmas Light",
+        url: "images/l3.jpg",
+        price: "CAD 15",
+        description:
+            "25 feet Colorful (4 colours- blue, pink, green, yellow) Chrismtas lights"
+    },
+
+    {
+        category: "l5",
+        alt: " Christmas Light",
+        url: "images/l5.jpg",
+        price: "CAD 25",
+        description: "Pack 10 strings of Window light : Star at end"
+    },
+    {
+        category: "l6",
+        alt: " Christmas Light",
+        url: "images/l6.jpg",
+        price: "CAD 15",
+        description: "5 feet Colorful round lights: Soothing"
+    }
+];
+var stars = [
+    {
+        category: "s1",
+        alt: "Christmas Star",
+        url: "images/s1.jpg",
+        price: "CAD 15",
+        description: "Pack of 5 hanging Star: Stuffed fabric"
+    },
+    {
+        category: "s2",
+        alt: " Christmas Star",
+        url: "images/s2.jpg",
+        price: "CAD 5",
+        description: "Golden Star: Tree Top (15cm * 10cm)"
+    },
+    {
+        category: "s3",
+        alt: " Christmas Star",
+        url: "images/s3.jpg",
+        price: "CAD 15",
+        description: "Pack of 5 hanging Star: Golden (4cm * 4cm)"
+    },
+    {
+        category: "s4",
+        alt: "Christmas Star",
+        url: "images/s4.jpg",
+        price: "CAD 20",
+        description: "Pack of 10 Wall sticker Star: Paper"
+    },
+    {
+        category: "s5",
+        alt: " Christmas Star",
+        url: "images/s5.jpg",
+        price: "CAD 20",
+        description: "Pack of 5 hanging Star: Silver (5cm * 5cm)"
+    },
+    {
+        category: "s6",
+        alt: " Christmas Star",
+        url: "images/s6.jpg",
+        price: "CAD 15",
+        description: "Pack of 10 hanging Santa Star: Paper"
+    }
+];
+var chocolates = [
+    {
+        category: "c1",
+        alt: "Christmas Chocolate",
+        url: "images/c1.jpg",
+        price: "CAD 55",
+        description: "12pieces Chocolate fudge: Net 150gm"
+    },
+    {
+        category: "c2",
+        alt: " Christmas Chocolate",
+        url: "images/c2.jpg",
+        price: "CAD 65",
+        description: "12pieces of assorted Chocolate: Net 175gm"
+    },
+    {
+        category: "c3",
+        alt: " Christmas Chocolate",
+        url: "images/c3.jpg",
+        price: "CAD 55",
+        description: "10pieces of Chocolate with Candy Canes: Net 250gm"
+    },
+    {
+        category: "c4",
+        alt: "Christmas Chocolate",
+        url: "images/c4.jpg",
+        price: "CAD 80",
+        description:
+            "10 Colourful Chocolate pieces with Candy Stick: Net 150 gm (size 8cm)"
+    },
+    {
+        category: "c5",
+        alt: " Christmas Chocolate",
+        url: "images/c5.jpg",
+
+        price: "CAD 100",
+        description: "10pieces,Chocolate fudge with Christmas Tree:150gm"
+    },
+    {
+        category: "c6",
+        alt: " Christmas Chocolate",
+        url: "images/c6.jpg",
+        price: "CAD 45",
+        description:
+            "12 pieces Chocolate Strawberry, Colourful decoration:Net 125gm "
+    }
+];
+var wreath = [
+    {
+        category: "b1",
+        alt: "Christmas Wreath",
+        url: "images/d1.jpg",
+        price: "CAD 65",
+        description: "24 Inch with Red decorative Cristmas balls"
+    },
+    {
+        category: "b2",
+        alt: " Christmas Wreath",
+        url: "images/d2.jpg",
+        price: "CAD 55",
+        description: " 13 inches Pine Garland with Colour Ball "
+    },
+    {
+        category: "b3",
+        alt: " Christmas Wreath",
+        url: "images/d3.jpg",
+        price: "CAD 60",
+        description: "Crestwood Spruce Wreath with 50 Clear Lights"
+    },
+    {
+        category: "b4",
+        alt: "Christmas Wreath",
+        url: "images/d4.jpg",
+        price: "CAD 55",
+        description: "18 Inch with decorative pine and red balls"
+    },
+    {
+        category: "b5",
+        alt: " Christmas Wreath",
+        url: "images/d5.jpg",
+
+        price: "CAD 60",
+        description: "20 Inch with medium decoration Pine"
+    }
+];
+var ball = [
+    {
+        category: "b1",
+        alt: "Christmas Ball",
+        url: "images/b1.jpg",
+        price: "CAD 30",
+        description: "Pack of 10 Christmas Balls: Red and White"
+    },
+    {
+        category: "b2",
+        alt: " Christmas Ball",
+        url: "images/b2.jpg",
+        price: "CAD 30",
+        description:
+            "Pack of 10 Christmas Balls: Green and Blue with crystal shine"
+    },
+    {
+        category: "b5",
+        alt: " Christmas Ball",
+        url: "images/b5.jpg",
+        price: "CAD 40",
+        description: "Pack of 10 Pink Christmas Balls"
+    },
+    {
+        category: "b4",
+        alt: "Christmas Ball",
+        url: "images/b4.jpg",
+        price: "CAD 40",
+        description: "Pack of 10 Assorted Christmas Balls"
+    },
+    {
+        category: "b3",
+        alt: " Christmas Ball",
+        url: "images/b3.jpg",
+        price: "CAD 40",
+        description:
+            "Pack of 8 Christmas Balls:  colour (blue and green) with gliter"
+    },
+
+    {
+        category: "b6",
+        alt: " Christmas Ball",
+        url: "images/b6.jpg",
+        price: "CAD 45",
+        description:
+            "Pack of 8 Crystal Christmas Balls, assorted, transparent. Colour- golden ,blue and copper"
+    }
+];
+/* About Us information div*/
+
+function showAboutUs() {
+    var dynamicDiv = document.querySelector("#dynamicDiv");
+    dynamicDiv.innerHTML = "";
+    dynamicDiv.classList.remove("hidden");
+    var Dy_single_box = document.createElement("div");
+    var pageTitle = document.createElement("h1");
+    pageTitle.innerHTML = "About Us";
+    dynamicDiv.appendChild(pageTitle);
+    Dy_single_box.classList.add("aboutUs");
+    var abtUs = document.createElement("p");
+    abtUs.innerHTML =
+        "WishTree is an Ontario based online shopping company. Our products are locally produced in partnership with the craftsmen community in the Greater Toronto Area. Through our outlet the craftsmen can showcase their skills and customers can shop for all Christmas related products. We specialize in Christmas decoration material, Christmas Trees, colorful Christmas Lights, and delicious Christmas Chocolates. Customer delight is our motto, and we assure speedy delivery to all provinces in Canada.";
+    Dy_single_box.appendChild(abtUs);
+    var add = document.createElement("span");
+
+    add.innerHTML =
+        "WishTree " +
+        "<br>" +
+        "ABC Avenue E," +
+        "<br>" +
+        "North York, ON MmM nNn" +
+        "<br>" +
+        "+1 (111) 111-1111" +
+        "<br>" +
+        "abc@whishtree.com";
+
+    Dy_single_box.appendChild(add);
+
+    dynamicDiv.appendChild(Dy_single_box);
+}
+/* Dynamic div for sub-categories*/
+
+function show(value) {
+    var dynamicDiv = document.querySelector("#dynamicDiv");
+    dynamicDiv.innerHTML = "";
+    dynamicDiv.classList.remove("hidden");
+
+    for (var i = 0; i < value.length; i++) {
+        var Dy_single_box = document.createElement("div");
+        Dy_single_box.classList.add("d-single-box");
+
+        var Dy_img_area = document.createElement("div");
+        Dy_img_area.classList.add("d-img-area");
+
+        var Dimg = document.createElement("img");
+        Dimg.src = value[i].url;
+        Dimg.width = 180;
+
+        var p = document.createElement("span");
+        p.innerHTML = value[i].description;
+
+        var price = document.createElement("p");
+        price.innerHTML = "<strong>" + value[i].price + "</strong>";
+
+        var submit = document.createElement("button");
+        submit.innerHTML = "Buy Now";
+        submit.id = "button";
+
+        submit.height = 30;
+        Dy_img_area.appendChild(Dimg);
+
+        Dy_single_box.appendChild(Dy_img_area);
+        Dy_single_box.appendChild(p);
+        Dy_single_box.appendChild(price);
+        Dy_single_box.appendChild(submit);
+
+        dynamicDiv.appendChild(Dy_single_box);
+    }
+}
+/*Event handling on categories */
+
+var box = document.querySelector("#div-items");
+var lightClick = document.querySelector("#light");
+lightClick.onclick = function() {
+    box.classList.add("hidden");
+    show(lights);
+};
+
+var treeClick = document.querySelector("#tree");
+treeClick.onclick = function() {
+    box.classList.add("hidden");
+    show(tree);
+};
+var starClick = document.querySelector("#star");
+starClick.onclick = function() {
+    box.classList.add("hidden");
+    show(stars);
+};
+var chocolatesClick = document.querySelector("#chocolate");
+chocolatesClick.onclick = function() {
+    box.classList.add("hidden");
+    show(chocolates);
+};
+var ballClick = document.querySelector("#ball");
+ballClick.onclick = function() {
+    box.classList.add("hidden");
+    show(ball);
+};
+
+var wreathClick = document.querySelector("#wreath");
+wreathClick.onclick = function() {
+    box.classList.add("hidden");
+    show(wreath);
+};
+
+/*Left navigation bar onclick events*/
+var aboutUsClick = document.querySelector("#aboutUs");
+aboutUsClick.onclick = function() {
+    box.classList.add("hidden");
+    showAboutUs();
+};
+var wreath1Click = document.querySelector("#wreath1");
+wreath1Click.onclick = function() {
+    box.classList.add("hidden");
+    show(wreath);
+};
+var ball1Click = document.querySelector("#ball1");
+ball1Click.onclick = function() {
+    box.classList.add("hidden");
+    show(ball);
+};
+var chocolates1Click = document.querySelector("#chocolate1");
+chocolates1Click.onclick = function() {
+    box.classList.add("hidden");
+    show(chocolates);
+};
+var tree1Click = document.querySelector("#tree1");
+tree1Click.onclick = function() {
+    box.classList.add("hidden");
+    show(tree);
+};
+var light1Click = document.querySelector("#light1");
+light1Click.onclick = function() {
+    box.classList.add("hidden");
+    show(lights);
+};
+var star1Click = document.querySelector("#star1");
+star1Click.onclick = function() {
+    box.classList.add("hidden");
+    show(stars);
+};
